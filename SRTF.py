701b76aa7889ba483f1820e461446e02b4d78a89
Mateeullah <bsef15a540@pucit.edu.pk>
2017-05-07 04:59:31 +0500

    Update SRTF.py

--- SRTF.py
+++ SRTF.py
@@ -1,31 +1,92 @@
 
-n= int(input('Enter the Number of processes'))
-at=[]
-wt=[]
-tat=[]
-bt=[]
-bt.insert(0,0)
-wt.insert(0,0)
-tat.insert(0,0)
-avgwt=0
-avgtat=0
+class Process:
+    def __init__(self,name,arrival_time,burst_time):
+        self.name=name
+        self.arrival_time=arrival_time
+        self.burst_time=burst_time
+        self.remaining_time=burst_time
+        self.add_status=0
+        self.waitingtime=None
+        self.turnaroundtime=None
 
-for i in range(1,n+1):
-    at.insert(i,i)
-    wt.insert(i,0)
-    bt.insert(i,int(input('Enter Burst time for process',i)))
+    def __repr__(self):
+        return '{}-{}'.format(self.name,self.arrival_time,self.waitingtime,self.turnaroundtime)
 
 
-for i in range(1,n+1):
-    bt.sort()
-    wt.append(wt[i-1]+bt[i-1])
-    tat.append(wt[i]+bt[i])
-    avgwt+=wt[i]
-    avgtat+=tat[i]
 
-avgwt/=n
-avgtat/=n
 
-print('average waiting time is: ',avgwt)
-print('average turnaround time is: ',avgtat)
+
+ def remaining_time_sort(Process):
+    return Process.remaining_time
+
+n=int(input('Enter the nuber of processes') )
+TimeQuantum=int(input('Enter the time Quantum'))
+
+Processes=[]
+for i in range (n):
+    arrivaltime = int(input('Enter the arrival time for process: ',i))
+    bursttime = int(input('Enter the burst time for process: ',i))
+    P=Process(i,arrivaltime,bursttime)
+    Processes.append(P)
+
+avg_waiting_time=0
+avg_turnaround_time=0
+Operations=True
+time=0
+current=0
+while (Operations==True):
+    increment_current = True
+    if(current>=n):
+        current=0
+    if(time>=Processes[current].arrivaltime and Processes[current].remaining_time>0):
+        increment_current=False
+        for i in range (TimeQuantum):
+            Processes[current].remaining_time-=1
+            time+=1
+            if(Processes[current].remaining_time==0):
+                Processes[current].waiting_time = time - Processes[current].arrival_time - Processes[current].burst_time
+                Processes[current].turnaroundtime = time - Processes[current].arrival_time
+                avg_waiting_time += Processes[current].waiting_time
+                avg_turnaround_time += Processes[current].turnaround_time
+                Processes.remove(current)
+        Processes.sort(key=remaining_time_sort)
+        current=0
+
+
+    for i in range(n):
+        if(len(Processes)==0):
+            Operations=False
+            break
+        if (Processes[i].remaining_time > 0):
+            Operations=True
+            break
+        else:
+            Operations = False
+    if(increment_current == True):
+        current+=1
+
+
+
+
+
+avg_waiting_time/=n
+avg_turnaround_time/=n
+
+print('Average Waiting Time = ',avg_waiting_time)
+print('Average Turnaround Time = ',avg_turnaround_time)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
