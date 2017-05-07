8ae3654b080a97d54145ae45803ace06b6081a3b
Mateeullah <bsef15a540@pucit.edu.pk>
2017-05-07 05:08:28 +0500

    Update SJF.py

--- SJF.py
+++ SJF.py
@@ -1,29 +1,29 @@
 #ASSUME ALL PROCESSES ARRIVED AT TIME 0
 n= int(input('Enter the Number of processes'))
-wt=[]
-tat=[]
-bt=[]
-bt.insert(0,0)
-wt.insert(0,0)
-tat.insert(0,0)
-avgwt=0
-avgtat=0
+waiting_time=[]
+turnaround_time=[]
+burst_time=[]
+burst_time.insert(0,0)
+waiting_time.insert(0,0)
+turnaround_time.insert(0,0)
+avg_waiting_time=0
+avg_turnaround_time=0
 
 for i in range(1,n+1):
-    wt.insert(i,0)
-    bt.insert(i,int(input('Enter Burst time for process',i)))
+    waiting_time.insert(i,0)
+    burst_time.insert(i,int(input('Enter Burst time for process',i)))
 
-bt.sort()
+burst_time.sort()
 
 for i in range(1,n+1):
-    wt.append(wt[i-1]+bt[i-1])
-    tat.append(wt[i]+bt[i])
-    avgwt+=wt[i]
-    avgtat+=tat[i]
+     waiting_time.append( waiting_time[i-1]+burst_time[i-1])
+    turnaround_time.append( waiting_time[i]+burst_time[i])
+    avg_waiting_time+= waiting_time[i]
+    avg_turnaround_time+=turnaround_time[i]
 
-avgwt/=n
-avgtat/=n
+avg_waiting_time/=n
+avg_turnaround_time/=n
 
-print('average waiting time is: ',avgwt)
-print('average turnaround time is: ',avgtat)
+print('average waiting time is: ',avg_waiting_time)
+print('average turnaround time is: ',avg_turnaround_time)
 
